4c087c2adb504cd87f12d9478909eb51877ccd3a
Visnu Pitiyanuvath <visnup@gmail.com>
2024-07-18 15:16:58 -0700

    Handle fuel and material cartel

--- src/player-board.ts
+++ src/player-board.ts
@@ -8,69 +8,42 @@ import {
 import type { Ambition, MapBoard } from "./map-board";
 const refObject = _refObject;
 
-// Detection zone
+// Board zone
+const p = refObject.getPosition();
 const { x, y } = refObject.getSize();
 const captivePercent = 0.735;
 const zoneId = `zone-${refObject.getId()}`;
 const zone =
   world.getZoneById(zoneId) ??
-  world.createZone(
-    refObject
-      .getPosition()
-      .add(new Vector(0, (-y * (1 - captivePercent)) / 2, 0)),
-  );
+  world.createZone(p.add(new Vector(0, (-y * (1 - captivePercent)) / 2, 0)));
 zone.setId(zoneId);
 zone.setRotation(refObject.getRotation());
 zone.setScale(new Vector(x, y * captivePercent, 20));
 zone.onBeginOverlap.add(updateAmbitions);
 zone.onEndOverlap.add(updateAmbitions);
 // Captives zone
-const captiveZoneId = `captive-zone-${refObject.getId()}`;
+const captiveZoneId = `zone-captive-${refObject.getId()}`;
 const captiveZone =
   world.getZoneById(captiveZoneId) ??
-  world.createZone(
-    refObject.getPosition().add(new Vector(0, (y * captivePercent) / 2, 0)),
-  );
+  world.createZone(p.add(new Vector(0, (y * captivePercent) / 2, 0)));
 captiveZone.setId(captiveZoneId);
 captiveZone.setRotation(refObject.getRotation());
 captiveZone.setScale(new Vector(x, y * (1 - captivePercent), 20));
 captiveZone.onBeginOverlap.add(updateAmbitions);
 captiveZone.onEndOverlap.add(updateAmbitions);
-
-// Card holders
-const holders = world
-  .getAllObjects()
-  .filter(
-    (obj) =>
-      obj instanceof CardHolder &&
-      !obj.getOnlyOwnerTakesCards() &&
-      obj.getOwningPlayerSlot() === refObject.getOwningPlayerSlot(),
-  ) as CardHolder[];
-for (const holder of holders) {
-  holder.onCardFlipped.add(updateAmbitions);
-  holder.onInserted.add(updateAmbitions);
-  holder.onRemoved.add(updateAmbitions);
-}
-
-const courtSuits: (Ambition | undefined)[] = [
-  "tycoon",
-  "tycoon",
-  ,
-  "empath",
-  "keeper",
-];
-function getHolderAmbitions(holder: CardHolder) {
-  const ambitions = { tycoon: 0, tyrant: 0, warlord: 0, keeper: 0, empath: 0 };
-  for (const card of holder.getCards()) {
-    // hack: use the index in the base court deck to infer the resource type
-    // 0-9 (0-1): tycoon, 15-19 (3): empath, 20-24 (4): keeper
-    if (holder.isCardFaceUp(card)) {
-      const suit = courtSuits[Math.floor(card.getCardDetails().index / 5)];
-      if (suit && suit in ambitions) ambitions[suit]++;
-    }
-  }
-  return new Map(Object.entries(ambitions) as [Ambition, number][]);
-}
+// Court zone
+const courtZoneHeight = 20;
+const courtZoneId = `zone-court-${refObject.getId()}`;
+const courtZone =
+  world.getZoneById(courtZoneId) ??
+  world.createZone(
+    p.add(new Vector(Math.sign(p.x) * ((x + courtZoneHeight) / 2), 0, 0)),
+  );
+courtZone.setId(courtZoneId);
+courtZone.setRotation(refObject.getRotation());
+courtZone.setScale(new Vector(courtZoneHeight, y, 20));
+courtZone.onBeginOverlap.add(updateAmbitions);
+courtZone.onEndOverlap.add(updateAmbitions);
 
 function updateAmbitions() {
   const ambitions = { tycoon: 0, tyrant: 0, warlord: 0, keeper: 0, empath: 0 };
@@ -107,15 +80,42 @@ function updateAmbitions() {
         break;
     }
   }
+
+  // Captive agents
   for (const obj of captiveZone.getOverlappingObjects()) {
     if (obj.getOwningPlayerSlot() === refObject.getOwningPlayerSlot()) continue;
     if (obj.getTemplateName() === "agent") ambitions.tyrant++;
   }
 
-  // Court cards
-  for (const court of holders)
-    for (const [ambition, count] of getHolderAmbitions(court))
-      ambitions[ambition] += count;
+  // Court cards, resources
+  const courtSuits: (Ambition | undefined)[] = [
+    "tycoon",
+    "tycoon",
+    ,
+    "empath",
+    "keeper",
+  ];
+  for (const obj of courtZone.getOverlappingObjects()) {
+    switch (obj.getTemplateName()) {
+      case "bc":
+        // hack: use the index in the base court deck to infer the resource type
+        // 0-9 (0-1): tycoon, 15-19 (3): empath, 20-24 (4): keeper
+        const suit =
+          courtSuits[Math.floor((obj as Card).getCardDetails().index / 5)];
+        if (suit && suit in ambitions) ambitions[suit]++;
+        break;
+      case "resource":
+        switch ((obj as Card).getCardDetails().name) {
+          case "fuel":
+          case "material":
+            obj.onDestroyed.clear();
+            obj.onDestroyed.add(updateAmbitions);
+            ambitions.tycoon += (obj as Card).getStackSize();
+            break;
+        }
+        break;
+    }
+  }
 
   // Update the ambitions on the map
   const map = world.getObjectById("map")! as MapBoard;
--- src/player-cards-holder.ts
+++ src/player-cards-holder.ts
@@ -1,10 +1,3 @@
 import { refHolder } from "@tabletop-playground/api";
 
-refHolder.onInserted.add((holder, card, player, inserted) => {
-  if (holder.getOnlyOwnerTakesCards()) {
-    // hand: todo sort action cards
-  } else {
-    // court: ensure correct orientation
-    if (Math.abs(card.getRotation().yaw) > 90) holder.rotateCard(card);
-  }
-});
+// todo: sort action cards
