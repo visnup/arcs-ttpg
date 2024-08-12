bb671270efc289e2f0df219eeab796dc8eca7cfb
Visnu Pitiyanuvath <visnup@gmail.com>
2024-08-11 19:34:48 -0700

    building primary action flips

--- src/building-tile.ts
+++ src/building-tile.ts
@@ -2,3 +2,7 @@ import { refCard } from "@tabletop-playground/api";
 
 // Set owning player slots by card image index
 refCard.setOwningPlayerSlot(refCard.getCardDetails().index);
+
+refCard.onPrimaryAction.add((card) => {
+  card.flipOrUpright();
+});
