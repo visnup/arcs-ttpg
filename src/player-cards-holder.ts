11a1c43298a879ab369751feca0e0e267c421324
Visnu Pitiyanuvath <visnup@gmail.com>
2024-09-10 00:28:40 -0700

    fix player hand sorting with campaign cards

--- src/player-cards-holder.ts
+++ src/player-cards-holder.ts
@@ -3,7 +3,11 @@ import { refHolder } from "@tabletop-playground/api";
 // Sort action cards
 (refHolder as any).sort = function (this: typeof refHolder) {
   for (const [i, card] of this.getCards()
-    .sort((a, b) => a.getCardDetails(0)!.index - b.getCardDetails(0)!.index)
+    .sort(
+      (a, b) =>
+        a.getTemplateName().localeCompare(b.getTemplateName()) ||
+        a.getCardDetails(0)!.index - b.getCardDetails(0)!.index,
+    )
     .entries())
     this.moveCard(card, i);
 };
