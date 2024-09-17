2aa0175b557b7320fd35576f97d0923951bab362
Visnu Pitiyanuvath <visnup@gmail.com>
2024-09-16 19:41:34 -0700

    denser notes

--- src/note.ts
+++ src/note.ts
@@ -8,7 +8,7 @@ import {
 
 const text = new RichText();
 text.setFont("MarkerFelt-Thin.ttf");
-text.setFontSize(48);
+text.setFontSize(28);
 text.setText(refObject.getDescription());
 text.setTextColor(new Color(0.05, 0.05, 0.05, 1));
 const ui = new UIElement();
