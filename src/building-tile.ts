1745b394c4baacc39feeeceeb49628cfb80043ed
Visnu Pitiyanuvath <visnup@gmail.com>
2024-12-24 14:50:32 -0800

    don't need custom action

--- src/building-tile.ts
+++ src/building-tile.ts
@@ -3,15 +3,6 @@ import { refCard } from "@tabletop-playground/api";
 // Set owning player slots by card image index
 refCard.setOwningPlayerSlot(refCard.getCardDetails().index);
 
-if (refCard.getStackSize() === 1) {
-  refCard.onPrimaryAction.add(toggle);
-  refCard.onCustomAction.add(toggle);
-  refCard.addCustomAction(
-    "Toggle Damage",
-    "Toggle building damage from fresh to damaged or back",
-  );
-}
-
-function toggle(card: typeof refCard) {
-  card.flipOrUpright();
-}
+// Primary action flips too
+if (refCard.getStackSize() === 1)
+  refCard.onPrimaryAction.add((card) => card.flipOrUpright());
