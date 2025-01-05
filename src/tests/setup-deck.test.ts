edf6c60b8d87603a8eaf879941465414a8bd6da2
Visnu Pitiyanuvath <visnup@gmail.com>
2025-01-04 16:15:50 -0800

    more setup tests

--- src/tests/setup-deck.test.ts
+++ src/tests/setup-deck.test.ts
@@ -1,12 +1,12 @@
-import type { Card } from "@tabletop-playground/api";
+import type { Card, GameObject } from "@tabletop-playground/api";
 import { ObjectType, world } from "@tabletop-playground/api";
 import type { TestableCard } from "../setup-deck";
 import { assertEqual, assertNotEqual } from "./assert";
 import { describe, test } from "./suite";
 
-function getCounts() {
+function getCounts(filter = (obj: GameObject) => !!obj) {
   const counts: Record<string, Record<string, number>> = {};
-  for (const obj of world.getAllObjects()) {
+  for (const obj of world.getAllObjects().filter(filter)) {
     const slot = (counts[obj.getOwningPlayerSlot()] ||= {});
     slot[obj.getTemplateName()] = (slot[obj.getTemplateName()] || 0) + 1;
   }
@@ -51,19 +51,72 @@ describe("setup deck", () => {
       "campaign-rules",
     ])
       assertEqual(counts[-1][key], undefined, `${key} deleted`);
-    // TODO court dealt
+    // court dealt
+    const court = world.getObjectByTemplateName("court")!;
+    assertEqual(
+      court.getAllSnapPoints().filter((d) => d.getSnappedObject()).length,
+      5,
+      "4 court cards dealt",
+    );
 
     // flip
     setup.flipOrUpright();
     setup.onFlipUpright.trigger(setup);
-    // TODO map annotated
+    // map annotated
+    assertEqual(world.getDrawingLines().length, 12, "map annotated");
+    assertEqual(
+      world.getObjectsByTemplateName("block").length,
+      1,
+      "block previewed",
+    );
 
     // run
     setup.onPrimaryAction.trigger(setup);
-    // TODO ships, buildings placed
-    // TODO resources drawn
-    // TODO blocks placed
-    // TODO ambition score indicators
+    // piece counts
+    const onMap = getCounts((obj) => world.isOnMap(obj));
+    for (const [slot, objects] of Object.entries({
+      "0": { ship: 8, power: 1, starport: 1, city: 1 },
+      "1": { ship: 8, power: 1, starport: 1, city: 1 },
+      "2": { ship: 8, power: 1, starport: 1, city: 1 },
+      "3": { ship: 8, power: 1, starport: 1, city: 1 },
+      "-1": {
+        block: 1,
+        "block small": 1,
+        "block round": 3,
+        chapter: 1,
+        ambition: 3,
+        "ambition declared": 1,
+      },
+    }))
+      for (const [name, count] of Object.entries(objects))
+        assertEqual(onMap[slot][name], count, `${slot} - ${name} on map`);
+    // resources drawn
+    for (const slot of [0, 1, 2, 3]) {
+      const board = world
+        .getObjectsByTemplateName("board")
+        .find((d) => d.getOwningPlayerSlot() === slot)!;
+      assertEqual(
+        JSON.stringify(
+          [
+            ...new Set(
+              board.getAllSnapPoints().map((d) => d.getSnappedObject()),
+            ),
+          ]
+            .map((d) => d?.getTemplateName())
+            .sort(),
+        ),
+        JSON.stringify([
+          "city",
+          "city",
+          "city",
+          "city",
+          "resource",
+          "resource",
+          null,
+        ]),
+        `board pieces ${slot}`,
+      );
+    }
 
     await new Promise((resolve) => setTimeout(resolve, 0));
     assertEqual(map.getObjectType(), ObjectType.Ground, "map is grounded");
@@ -82,6 +135,33 @@ describe("setup deck", () => {
     // piece counts
     const counts = getCounts();
     assertEqual(Object.keys(counts), "0 1 2 -1".split(" "), "player pieces");
+    // court dealt
+    const court = world.getObjectByTemplateName("court")!;
+    assertEqual(
+      court.getAllSnapPoints().filter((d) => d.getSnappedObject()).length,
+      5,
+      "4 court cards dealt",
+    );
+
+    // run
+    setup.onPrimaryAction.trigger(setup);
+    // piece counts
+    const onMap = getCounts((obj) => world.isOnMap(obj));
+    for (const [slot, objects] of Object.entries({
+      "0": { ship: 8, power: 1, starport: 1, city: 1 },
+      "1": { ship: 8, power: 1, starport: 1, city: 1 },
+      "2": { ship: 8, power: 1, starport: 1, city: 1 },
+      "-1": {
+        block: 2,
+        "block small": 2,
+        "block round": 6,
+        chapter: 1,
+        ambition: 3,
+        "ambition declared": 1,
+      },
+    }))
+      for (const [name, count] of Object.entries(objects))
+        assertEqual(onMap[slot][name], count, `${slot} - ${name} on map`);
   });
 
   test("2p", () => {
@@ -97,9 +177,33 @@ describe("setup deck", () => {
     // piece counts
     const counts = getCounts();
     assertEqual(Object.keys(counts), "0 1 -1".split(" "), "player pieces");
+    // court dealt
+    const court = world.getObjectByTemplateName("court")!;
+    assertEqual(
+      court.getAllSnapPoints().filter((d) => d.getSnappedObject()).length,
+      4,
+      "3 court cards dealt",
+    );
 
     // run
     setup.onPrimaryAction.trigger(setup);
+    // piece counts
+    const onMap = getCounts((obj) => world.isOnMap(obj));
+    for (const [slot, objects] of Object.entries({
+      "0": { ship: 10, power: 1, starport: 1, city: 1 },
+      "1": { ship: 10, power: 1, starport: 1, city: 1 },
+      "-1": {
+        block: 2,
+        "block large": 2,
+        "block round": 6,
+        chapter: 1,
+        ambition: 3,
+        "ambition declared": 1,
+        resource: 4,
+      },
+    }))
+      for (const [name, count] of Object.entries(objects))
+        assertEqual(onMap[slot][name], count, `${slot} - ${name} on map`);
     // blocked resources
     const blockedResources = world
       .getObjectsByTemplateName<Card>("resource")
