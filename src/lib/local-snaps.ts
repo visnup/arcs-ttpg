e535301d6090b3251c56f3122fc691b66f694cb2
Visnu Pitiyanuvath <visnup@gmail.com>
2025-01-29 13:47:08 -1000

    local snap state survives saves

--- src/lib/local-snaps.ts
+++ src/lib/local-snaps.ts
@@ -3,20 +3,28 @@ import { world, ZonePermission } from "@tabletop-playground/api";
 
 // When an object to snapped to a "local" snap point, disable it within a zone
 // matching the snap area until all objects have exited the zone.
+const { Everybody, Nobody } = ZonePermission;
 export function localSnaps(obj: GameObject) {
   const local = obj
     .getAllSnapPoints()
-    .filter((s) => s.getTags().includes("local"));
+    .filter((s) => s.getTags().includes("local"))
+    .sort((a, b) => a.getLocalPosition().y - b.getLocalPosition().y);
   for (const [i, snap] of local.entries()) {
     const zone = world.createZone(snap.getGlobalPosition());
     zone.setId(`zone-snap-${obj.getId()}-${i}`);
     zone.setScale([0.1, 0.1, 0.1]);
+    zone.setSnapping(JSON.parse(obj.getSavedData(zone.getId()) || "0"));
     zone.onBeginOverlap.add((zone, o) => {
-      if (o !== obj) zone.setSnapping(ZonePermission.Nobody);
+      if (o !== obj) {
+        zone.setSnapping(Nobody);
+        obj.setSavedData(JSON.stringify(Nobody), zone.getId());
+      }
     });
     zone.onEndOverlap.add((zone, o) => {
-      if (o !== obj && zone.getOverlappingObjects().length <= 1)
-        zone.setSnapping(ZonePermission.Everybody);
+      if (o !== obj && zone.getOverlappingObjects().length <= 1) {
+        zone.setSnapping(Everybody);
+        obj.setSavedData(JSON.stringify(Everybody), zone.getId());
+      }
     });
     obj.onDestroyed.add(() => zone.destroy());
   }
