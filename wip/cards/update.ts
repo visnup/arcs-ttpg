45fd917e75c206b61997e9d0c0677c6ed425c23e
Visnu Pitiyanuvath <visnup@gmail.com>
2025-02-08 22:49:09 -0800

    run card update from yarn

--- wip/cards/update.ts
+++ wip/cards/update.ts
@@ -2,20 +2,20 @@ import { cp, readFile, writeFile } from "fs/promises";
 import { join } from "path";
 import { parse } from "yaml";
 
-const src = (...s: string[]) => join("../../../cards/content", ...s);
+const src = (...s: string[]) => join("../cards/content", ...s);
 
 await Promise.all([
-  cp(src("card-data/arcs/en-US"), ".", { recursive: true }),
-  cp(src("faq/arcs/en-US.yml"), "faq.yml"),
-  cp(src("errata/arcs/en-US.yml"), "errata.yml"),
+  cp(src("card-data/arcs/en-US"), "wip/cards", { recursive: true }),
+  cp(src("faq/arcs/en-US.yml"), "wip/cards/faq.yml"),
+  cp(src("errata/arcs/en-US.yml"), "wip/cards/errata.yml"),
 ]);
 
-const base = parse(await readFile("arcsbasegame.yml", "utf8"));
-const leaders = parse(await readFile("leaders-lore.yml", "utf8"));
-const campaign = parse(await readFile("blightedreach.yml", "utf8"));
+const base = parse(await readFile("wip/cards/arcsbasegame.yml", "utf8"));
+const leaders = parse(await readFile("wip/cards/leaders-lore.yml", "utf8"));
+const campaign = parse(await readFile("wip/cards/blightedreach.yml", "utf8"));
 // const errata = parse(await readFile("errata.yml", "utf8"));
 const faq = new Map(
-  parse(await readFile("faq.yml", "utf8")).map(({ card, faq }) => [
+  parse(await readFile("wip/cards/faq.yml", "utf8")).map(({ card, faq }) => [
     card,
     faq
       .map(({ q, a }) => `Q: ${q}\nA: ${a}\n`)
@@ -45,49 +45,49 @@ function names(cards: any[], filter: (d: any) => boolean) {
 }
 
 // setup.json
-modify("../../assets/Templates/cards/setup.json", (json) => {
+modify("assets/Templates/cards/setup.json", (json) => {
   json["CardNames"] = names(base, (d) => d.id.match(/^ARCS-\dSETUP/));
   return json;
 });
 
 // bc.json: base court
-modify("../../assets/Templates/cards/bc.json", (json) => {
+modify("assets/Templates/cards/bc.json", (json) => {
   json["CardNames"] = names(base, (d) => d.id.startsWith("ARCS-BC"));
   return json;
 });
 
 // leader.json
-modify("../../assets/Templates/cards/leader.json", (json) => {
+modify("assets/Templates/cards/leader.json", (json) => {
   json["CardNames"] = names(base, (d) => d.id.startsWith("ARCS-LEAD"));
   return json;
 });
 
 // lore.json
-modify("../../assets/Templates/cards/lore.json", (json) => {
+modify("assets/Templates/cards/lore.json", (json) => {
   json["CardNames"] = names(base, (d) => d.id.match(/^ARCS-L\d+$/));
   return json;
 });
 
 // leader-2.json
-modify("../../assets/Templates/cards/leader-2.json", (json) => {
+modify("assets/Templates/cards/leader-2.json", (json) => {
   json["CardNames"] = names(leaders, (d) => d.id.startsWith("ARCS-LEAD"));
   return json;
 });
 
 // lore-2.json
-modify("../../assets/Templates/cards/lore-2.json", (json) => {
+modify("assets/Templates/cards/lore-2.json", (json) => {
   json["CardNames"] = names(leaders, (d) => d.id.match(/^ARCS-L\d+$/));
   return json;
 });
 
 // cc.json: campaign court
-modify("../../assets/Templates/campaign/cc.json", (json) => {
+modify("assets/Templates/campaign/cc.json", (json) => {
   json["CardNames"] = names(campaign, (d) => d.id.startsWith("ARCS-CC"));
   return json;
 });
 
 // dc.json
-modify("../../assets/Templates/campaign/dc.json", (json) => {
+modify("assets/Templates/campaign/dc.json", (json) => {
   json["CardNames"] = names(campaign, (d) => d.id.match(/^ARCS-AID\d+A?$/));
   // 10, 11, 12, 13 = flagship upgrades
   for (const i of [10, 11, 12, 13]) json["CardNames"][i] = json["CardNames"][7];
@@ -97,7 +97,7 @@ modify("../../assets/Templates/campaign/dc.json", (json) => {
 });
 
 // fate.json
-modify("../../assets/Templates/campaign/fate.json", (json) => {
+modify("assets/Templates/campaign/fate.json", (json) => {
   json["CardNames"] = names(campaign, (d) => d.id.startsWith("ARCS-FATE"));
   return json;
 });
@@ -105,7 +105,7 @@ modify("../../assets/Templates/campaign/fate.json", (json) => {
 // f01.json..f24.json
 for (let i = 1; i <= 24; i++) {
   const n = i.toString().padStart(2, "0");
-  modify(`../../assets/Templates/campaign/f${n}.json`, (json) => {
+  modify(`assets/Templates/campaign/f${n}.json`, (json) => {
     const n = names(campaign, (d) =>
       d.id.match(new RegExp(`^ARCS-F${i}\\d\\dA?$`)),
     );
