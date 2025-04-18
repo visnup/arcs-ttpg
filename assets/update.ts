c14515421a07c093b608656e4c9fcc8409170b36
Visnu Pitiyanuvath <visnup@gmail.com>
2025-04-18 01:02:50 -0700

    build leaders, lore, and fate card images

--- assets/update.ts
+++ assets/update.ts
@@ -13,9 +13,15 @@ await Promise.all([
   // cp(src("errata/arcs/en-US.yml"), "wip/cards/errata.yml"),
 ]);
 
-const base = parse(await readFile("wip/cards/arcsbasegame.yml", "utf8"));
-const leaders = parse(await readFile("wip/cards/leaders-lore.yml", "utf8"));
-const campaign = parse(await readFile("wip/cards/blightedreach.yml", "utf8"));
+const base: Card[] = parse(
+  await readFile("wip/cards/arcsbasegame.yml", "utf8"),
+);
+const leaders: Card[] = parse(
+  await readFile("wip/cards/leaders-lore.yml", "utf8"),
+);
+const campaign: Card[] = parse(
+  await readFile("wip/cards/blightedreach.yml", "utf8"),
+);
 // const errata = parse(await readFile("errata.yml", "utf8"));
 const faq = new Map(
   parse(await readFile("wip/cards/faq.yml", "utf8")).map(({ card, faq }) => [
@@ -40,27 +46,24 @@ async function modify(path: string, cb: (c: JsonObject) => JsonObject) {
 async function image(
   path: string,
   cards: Card[],
-  f = (c: Card) => c.image,
   columns = 7,
+  width = 4096,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  f = (c: Card, i: number) => c.image,
 ) {
   if (cards.length === 0) return;
 
   // Get dimensions of the first card and determine card sizing
   const metadata = await sharp(
-    src("card-images/arcs/en-US", `${f(cards[0])}.png`),
+    src("card-images/arcs/en-US", `${f(cards[0], 0)}.png`),
   ).metadata();
   if (!metadata.width || !metadata.height)
     throw new Error(`Could not determine dimensions of first card image`);
 
   // Determine card dimensions (scale if needed to fit within 4096px width)
-  const cardWidth =
-    metadata.width! * columns > 4096
-      ? Math.floor(4096 / columns)
-      : metadata.width!;
-  const cardHeight =
-    metadata.width! * columns > 4096
-      ? Math.floor((cardWidth * metadata.height!) / metadata.width!)
-      : metadata.height!;
+  const { width: w, height: h } = metadata;
+  const cardWidth = w * columns > width ? Math.floor(width / columns) : w;
+  const cardHeight = w * columns > width ? Math.floor((cardWidth * h) / w) : h;
 
   // Process all card images and create composite
   await sharp({
@@ -74,7 +77,9 @@ async function image(
     .composite(
       await Promise.all(
         cards.map(async (card, index) => ({
-          input: await sharp(src("card-images/arcs/en-US", `${f(card)}.png`))
+          input: await sharp(
+            src("card-images/arcs/en-US", `${f(card, index)}.png`),
+          )
             .resize(cardWidth, cardHeight, { fit: "fill" })
             .toBuffer(),
           top: Math.floor(index / columns) * cardHeight,
@@ -114,9 +119,8 @@ interface Card {
   flipSide?: string;
 }
 
-function names(cards: Card[], filter: (d: Card) => boolean) {
+function names(cards: Card[]) {
   return cards
-    .filter(filter)
     .sort((a, b) => a.id.localeCompare(b.id))
     .reduce(
       (acc, d, i) => ({
@@ -130,12 +134,10 @@ function names(cards: Card[], filter: (d: Card) => boolean) {
 }
 function abilities(
   cards: Card[],
-  filter: (d: Card) => boolean,
   previous: Record<string, string> | undefined,
 ) {
   if (!previous) return {};
   return cards
-    .filter(filter)
     .sort((a, b) => a.id.localeCompare(b.id))
     .reduce((acc, d, i) => {
       const append =
@@ -166,7 +168,7 @@ modify("assets/Templates/cards/setup.json", (json) => {
 
 // bc.json: base court
 modify("assets/Templates/cards/bc.json", (json) => {
-  json["CardNames"] = names(base, (d) => d.id.startsWith("ARCS-BC"));
+  json["CardNames"] = names(base.filter((d) => d.id.startsWith("ARCS-BC")));
   return json;
 });
 image(
@@ -176,18 +178,22 @@ image(
 
 // leader.json
 modify("assets/Templates/cards/leader.json", (json) => {
-  json["CardNames"] = names(base, (d) => d.id.startsWith("ARCS-LEAD"));
+  json["CardNames"] = names(base.filter((d) => d.id.startsWith("ARCS-LEAD")));
   json["CardMetadata"] = abilities(
-    base,
-    (d) => d.id.startsWith("ARCS-LEAD"),
+    base.filter((d) => d.id.startsWith("ARCS-LEAD")),
     json["CardMetadata"],
   );
   return json;
 });
+image(
+  "assets/Textures/cards/leader.jpg",
+  base.filter((d) => d.id.startsWith("ARCS-LEAD")),
+  4,
+);
 
 // lore.json
 modify("assets/Templates/cards/lore.json", (json) => {
-  json["CardNames"] = names(base, (d) => !!d.id.match(/^ARCS-L\d+$/));
+  json["CardNames"] = names(base.filter((d) => !!d.id.match(/^ARCS-L\d+$/)));
   return json;
 });
 image(
@@ -197,18 +203,24 @@ image(
 
 // leader-2.json
 modify("assets/Templates/cards/leader-2.json", (json) => {
-  json["CardNames"] = names(leaders, (d) => d.id.startsWith("ARCS-LEAD"));
+  json["CardNames"] = names(
+    leaders.filter((d) => d.id.startsWith("ARCS-LEAD")),
+  );
   json["CardMetadata"] = abilities(
-    leaders,
-    (d) => d.id.startsWith("ARCS-LEAD"),
+    leaders.filter((d) => d.id.startsWith("ARCS-LEAD")),
     json["CardMetadata"],
   );
   return json;
 });
+image(
+  "assets/Textures/cards/leader-2.jpg",
+  leaders.filter((d) => d.id.startsWith("ARCS-LEAD")),
+  4,
+);
 
 // lore-2.json
 modify("assets/Templates/cards/lore-2.json", (json) => {
-  json["CardNames"] = names(leaders, (d) => !!d.id.match(/^ARCS-L\d+$/));
+  json["CardNames"] = names(leaders.filter((d) => !!d.id.match(/^ARCS-L\d+$/)));
   return json;
 });
 image(
@@ -218,7 +230,7 @@ image(
 
 // cc.json: campaign court
 modify("assets/Templates/campaign/cc.json", (json) => {
-  json["CardNames"] = names(campaign, (d) => d.id.startsWith("ARCS-CC"));
+  json["CardNames"] = names(campaign.filter((d) => d.id.startsWith("ARCS-CC")));
   return json;
 });
 image(
@@ -228,7 +240,9 @@ image(
 
 // dc.json
 modify("assets/Templates/campaign/dc.json", (json) => {
-  json["CardNames"] = names(campaign, (d) => !!d.id.match(/^ARCS-AID\d+A?$/));
+  json["CardNames"] = names(
+    campaign.filter((d) => !!d.id.match(/^ARCS-AID\d+A?$/)),
+  );
   // 10, 11, 12, 13 = flagship upgrades
   for (const i of [9, 10, 11, 12]) json["CardNames"][i] = json["CardNames"][6];
   // 6, 7, 8, 9 = regent
@@ -238,25 +252,44 @@ modify("assets/Templates/campaign/dc.json", (json) => {
 
 // fate.json
 modify("assets/Templates/campaign/fate.json", (json) => {
-  json["CardNames"] = names(campaign, (d) => d.id.startsWith("ARCS-FATE"));
+  json["CardNames"] = names(
+    campaign.filter((d) => d.id.startsWith("ARCS-FATE")),
+  );
   return json;
 });
+image(
+  "assets/Textures/campaign/fate.jpg",
+  campaign.filter((d) => d.id.startsWith("ARCS-FATE")),
+  8,
+  8 * 800,
+);
+image(
+  "assets/Textures/campaign/fate-back.jpg",
+  campaign.filter((d) => d.id.startsWith("ARCS-FATE")),
+  8,
+  8 * 800,
+  (c, i) => ["FATEA", "FATEB", "FATEC"][Math.floor(i / 8)],
+);
 
 // f01.json..f24.json
 for (let i = 1; i <= 24; i++) {
   const n = i.toString().padStart(2, "0");
-  const fate = (d) => !!d.id.match(new RegExp(`^ARCS-F${i}\\d\\dA?$`));
+  const fate = campaign.filter(
+    (d) => !!d.id.match(new RegExp(`^ARCS-F${i}\\d\\dA?$`)),
+  );
   modify(`assets/Templates/campaign/f${n}.json`, (json) => {
-    const n = names(campaign, fate);
+    const n = names(fate);
     json["CardNames"] = Object.fromEntries(
       Object.entries(n).map(([i, v], _, r) => [r.length - 1 - +i, v]),
     ) as Record<string, string>;
     return json;
   });
-  image(`assets/Textures/campaign/f${n}.jpg`, campaign.filter(fate));
+  image(`assets/Textures/campaign/f${n}.jpg`, fate);
   image(
     `assets/Textures/campaign/f${n}b.jpg`,
-    campaign.filter(fate),
+    fate,
+    7,
+    4096,
     (c) => c.flipSide?.replace("ARCS-", "") ?? `F${i}`,
   );
 }
