e8183b11a04f4b0b1696e1f92b3020885ebaeba6
Visnu Pitiyanuvath <visnup@gmail.com>
2025-04-21 23:50:40 -0700

    custom action to raise power marker from bottom

--- src/power-marker.ts
+++ src/power-marker.ts
@@ -1,13 +1,19 @@
 import { refObject, world } from "@tabletop-playground/api";
 
 // Rotate stacked markers to grab one you want
+refObject.onPrimaryAction.add(rotateStack);
+refObject.onCustomAction.add(rotateStack);
+refObject.addCustomAction("Raise from bottom");
+
 const names = new Set(["power", "objective"]);
-refObject.onPrimaryAction.add((obj) => {
+function rotateStack(obj: typeof refObject) {
   const p = obj.getPosition();
+  const z = obj.getSize().z + 0.1;
   const stack = world
     .lineTrace(p.add([0, 0, -10]), p.add([0, 0, 10]))
     .map((d) => d.object)
     .filter((d) => names.has(d.getTemplateName()));
-  stack[0].setPosition(stack[stack.length - 1].getPosition().add([0, 0, 2]));
+  if (stack.length === 1) return;
+  stack[0].setPosition(stack[stack.length - 1].getPosition().add([0, 0, z]));
   for (const obj of [...stack.slice(1), stack[0]]) obj.snap();
-});
+}
