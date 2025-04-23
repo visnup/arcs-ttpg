c44606ff43686fad95b9dcc11f5a28dcbdf18573
Visnu Pitiyanuvath <visnup@gmail.com>
2025-04-22 17:39:44 -0700

    lift power markers from bottom

--- src/power-marker.ts
+++ src/power-marker.ts
@@ -1,4 +1,4 @@
-import { refObject, world } from "@tabletop-playground/api";
+import { ObjectType, refObject, world } from "@tabletop-playground/api";
 
 // Rotate stacked markers to grab one you want
 refObject.onPrimaryAction.add(rotateStack);
@@ -17,3 +17,19 @@ function rotateStack(obj: typeof refObject) {
   stack[0].setPosition(stack[stack.length - 1].getPosition().add([0, 0, z]));
   for (const obj of [...stack.slice(1), stack[0]]) obj.snap();
 }
+
+// Make picked up marker penetrable and release ones held above
+refObject.onGrab.add((obj) => {
+  const p = obj.getPosition();
+  if (
+    world
+      .lineTrace(p, p.add([0, 0, -10]))
+      .some((d) => d.object !== obj && d.object.isHeld())
+  ) {
+    obj.release();
+  } else {
+    const type = obj.getObjectType();
+    obj.setObjectType(ObjectType.Penetrable);
+    process.nextTick(() => obj.setObjectType(type));
+  }
+});
