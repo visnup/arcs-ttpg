1a522447cbca681e9b146ba9279d570b75807cad
Visnu Pitiyanuvath <visnup@gmail.com>
2025-05-12 21:27:00 -0700

    test blightkin

--- src/tests/set-tile.test.ts
+++ src/tests/set-tile.test.ts
@@ -29,7 +29,7 @@ describe("set tile", () => {
     });
   });
 
-  test.only("edenguard", async () => {
+  test("edenguard", async () => {
     const fates = world
       .getObjectsByTemplateName<Card>("fate")
       .sort((a, b) => a.getPosition().y - b.getPosition().y);
@@ -56,7 +56,7 @@ describe("set tile", () => {
     const firstRegent = world.getObjectByTemplateName("first-regent")!;
     assert(
       firstRegent.getPosition().distance(guardian.getPosition()) < 7,
-      "first regent with admiral",
+      "first regent with guardian",
     );
 
     // place edenguard
@@ -65,15 +65,15 @@ describe("set tile", () => {
     const map = world.getObjectByTemplateName("map")!;
     edenguard.setPosition(map.getPosition().add([0, 0, 1]));
 
-    // empire control
+    // imperial control
     const number = world.getObjectByTemplateName<Dice>("number")!;
     const imperial = [, 2, 2, 4, 2, 2, 4][+number.getCurrentFaceName()]!;
     assertEqual(
       ambitions.edenguard,
       { [slot]: imperial },
-      "initial empire control",
+      "initial imperial control",
     );
-    // empire control follows first regent
+    // imperial control follows first regent
     const boards = world.getObjectsByTemplateName("board")!;
     firstRegent.setPosition(
       boards
@@ -103,5 +103,88 @@ describe("set tile", () => {
     );
   });
 
-  test("blightkin", async () => {});
+  test("blightkin", async () => {
+    const fates = world
+      .getObjectsByTemplateName<Card>("fate")
+      .sort((a, b) => a.getPosition().y - b.getPosition().y);
+    if (fates.length === 0) skip("no fates");
+    // run setup
+    (fates[0] as TestableCard).onClick(4, fates[0]);
+
+    // assign naturalist to 1p
+    const slots = world.getSlots();
+    const slot = slots[0];
+    const guardian = fates[2].takeCards(1, false, 3)!;
+    guardian.setPosition(
+      world
+        .getObjectsByTemplateName("board")
+        .find((d) => d.getOwningPlayerSlot() === slots[0])!
+        .getAllSnapPoints()
+        .find((s) => s.getTags().includes("fate"))!
+        .getGlobalPosition()
+        .add([0, 0, 1]),
+    );
+    guardian.snap();
+    await new Promise((resolve) => setTimeout(resolve, 500)); // wait to avoid hang?
+    (guardian as TestableCard).onSnapped.trigger(guardian);
+    const firstRegent = world.getObjectByTemplateName("first-regent")!;
+    assert(
+      firstRegent.getPosition().distance(guardian.getPosition()) < 7,
+      "first regent with naturalist",
+    );
+
+    // place blightkin
+    const blightkin = world.getObjectByTemplateName("set-tile");
+    assert(blightkin !== undefined, "edenguard");
+    const map = world.getObjectByTemplateName("map")!;
+    blightkin.setPosition(map.getPosition().add([0, 0, 1]));
+
+    // imperial control
+    assertEqual(ambitions.blightkin, {}, "initial imperial control");
+    for (const s of getSystems())
+      if (s.snap.getTags().includes("system:0"))
+        placeShips(4, 1, s.snap.getGlobalPosition());
+    assertEqual(ambitions.blightkin, {}, "damaged blight");
+    for (const blight of world.getObjectsByTemplateName<Card>("set-round"))
+      if (
+        blight.getStackSize() === 1 &&
+        blight.getCardDetails().metadata === "blight"
+      )
+        blight.flipOrUpright();
+    await assertEqualEventually(
+      () => ambitions.blightkin,
+      { [slot]: 4 },
+      "fresh blight",
+    );
+
+    // imperial control follows first regent
+    const boards = world.getObjectsByTemplateName("board")!;
+    firstRegent.setPosition(
+      boards
+        .find((d) => d.getOwningPlayerSlot() === slots[1])!
+        .getPosition()
+        .add([0, -23, 1]),
+    );
+    await assertEqualEventually(
+      () => ambitions.blightkin,
+      { [slots[1]]: 4 },
+      "first regent moved",
+    );
+
+    // player control
+    // place 2p ships
+    for (const s of getSystems())
+      if (s.snap.getTags().includes("resource:fuel"))
+        placeShips(slot, 1, s.snap.getGlobalPosition());
+    globalEvents.onAmbitionShouldTally.trigger("blightkin");
+    const number = world.getObjectByTemplateName<Dice>("number")!;
+    assertEqual(
+      ambitions.blightkin,
+      {
+        [slots[1]]: 4,
+        [slot]: [, 3, 3, 2, 3, 3, 2][+number.getCurrentFaceName()]!,
+      },
+      "player control",
+    );
+  });
 });
