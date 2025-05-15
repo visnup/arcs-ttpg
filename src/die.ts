5011f70b8797bd7a740123c9876b44d23b0a079e
Visnu Pitiyanuvath <visnup@gmail.com>
2025-05-14 18:52:57 -0700

    cmd-r to add/remove dice from tray

--- src/die.ts
+++ src/die.ts
@@ -1,5 +1,25 @@
-import { refObject } from "@tabletop-playground/api";
+import { refDice, Vector, world } from "@tabletop-playground/api";
 import { discardToOrigin } from "./lib/discard-to-origin";
 
 // discardable
-discardToOrigin(refObject, false, false);
+discardToOrigin(refDice, false, false);
+
+// Add, remove from tray
+refDice.onSecondaryAction.add(toggle);
+
+const tray = world.getObjectByTemplateName("tray");
+function toggle(obj: typeof refDice) {
+  const zone = world.getZoneById(`zone-dice-${tray?.getId()}`);
+  if (!zone) return;
+  if (zone.isOverlapping(obj)) {
+    if ("discard" in obj && typeof obj.discard === "function") obj.discard();
+  } else {
+    obj.setPosition(
+      Vector.randomPointInBoundingBox(
+        zone.getPosition().add([0, 0, 1]),
+        zone.getScale().multiply(0.2),
+      ),
+      1.5,
+    );
+  }
+}
