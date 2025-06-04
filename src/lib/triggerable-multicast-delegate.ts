045b59632eaf4d9e47e91098c8968950fe8e7ed0
RobertGuglielmino <robert.gugliel@gmail.com>
2025-06-04 14:11:19 -0400

    event trigger throttling

--- src/lib/triggerable-multicast-delegate.ts
+++ src/lib/triggerable-multicast-delegate.ts
@@ -2,6 +2,10 @@ export class TriggerableMulticastDelegate<
   T extends (...args: never[]) => unknown,
 > {
   delegates = new Set<T>();
+  executionTimeMap = new WeakMap();
+  lastExecutionTime = 0;
+  THROTTLE_MS = 100;
+
   add(fn: T) {
     this.delegates.add(fn);
   }
@@ -12,6 +16,17 @@ export class TriggerableMulticastDelegate<
     this.delegates.clear();
   }
   trigger = (...args: Parameters<T>) => {
-    for (const fn of this.delegates) fn(...args);
+    const key: WeakKey = args;
+    const now = Date.now();
+
+    if (!this.executionTimeMap.has(key)) {
+      this.executionTimeMap.set(key, 0);
+    }
+
+    if (now - this.executionTimeMap.get(key) >= this.THROTTLE_MS) {
+      this.executionTimeMap.set(key, now);
+      for (const fn of this.delegates) fn(...args);
+    }
+    // If not enough time passed, ignore this event
   };
 }
