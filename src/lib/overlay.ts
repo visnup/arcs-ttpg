327de2127eec1531af84291026ad4333d387157c
Visnu Pitiyanuvath <visnup@gmail.com>
2025-06-20 13:35:23 +0900

    kinda working

--- src/lib/overlay.ts
+++ src/lib/overlay.ts
@@ -1,4 +1,9 @@
-import { world, type CardHolder } from "@tabletop-playground/api";
+import {
+  Card,
+  world,
+  type CardHolder,
+  type GameObject,
+} from "@tabletop-playground/api";
 import { type Ambition } from "../map-board";
 
 type GameData = {
@@ -12,20 +17,21 @@ type GameData = {
 type PlayerColor = "FFB700" | "0095A9" | "E1533D" | "D7D2CB" | "912AAD";
 type PlayerRank = [PlayerColor, number];
 type PlayerData = {
-  name: string;
+  name?: string;
   color: PlayerColor;
   initiative: boolean;
   power: number;
-  objective: number;
   outrage: string[];
   resources: string[];
   cities: number;
   spaceports: number;
   ships: number;
   agents: number;
-  favors: PlayerRank[];
   guild: string[];
-  titles: string[];
+  // campaign
+  objective?: number;
+  favors?: PlayerRank[];
+  titles?: string[];
 };
 type AmbitionData = {
   name: Ambition;
@@ -38,19 +44,56 @@ type CourtData = {
   // todo: attached
 };
 
+const isGuild = (d: GameObject): d is Card =>
+  d instanceof Card && /^(bc|cc|lore|f\d+)$/.test(d.getTemplateName());
+const cardName = (d: Card) => d.getCardDetails().name.replace(/\n.*/s, "");
+
 export function sync() {
-  const objects = world.getAllObjects();
   const players = world.getAllPlayers();
+  const objects = world.getAllObjects();
+  const initiative = world.getObjectById("initiative")!.getPosition();
+  const boards = objects.filter((d) => d.getTemplateName() === "board");
+  const hasInitiative = boards.sort(
+    (a, b) =>
+      a.getPosition().distance(initiative) -
+      b.getPosition().distance(initiative),
+  )[0];
   const rules = world.getObjectById("rules") as CardHolder | undefined;
 
   const data: GameData = {
     campaign: !!rules,
-    players: [],
-    ambitions: [],
-    court: [],
-    edicts: [],
-    laws: [],
+    players: boards.map((board) => {
+      const guild =
+        world
+          .getZoneById(`zone-player-court-${board.getId()}`)
+          ?.getOverlappingObjects()
+          .filter(isGuild)
+          .map(cardName) ?? [];
+      return {
+        name: players
+          .find((p) => p.getSlot() === board.getOwningPlayerSlot())
+          ?.getName(),
+        color: board.getPrimaryColor().toHex().slice(0, 6) as PlayerColor,
+        initiative: board === hasInitiative,
+        power: 0, // todo
+        outrage: [], // todo
+        resources: [], // todo
+        cities: 0, // todo
+        spaceports: 0, // todo
+        ships: 0, // todo
+        agents: 0, // todo
+        guild,
+        objective: 0, // todo
+        favors: [], // todo
+        titles: [], // todo
+      };
+    }),
+    ambitions: [], // todo
+    court: [], // todo
+    edicts: [], // todo
+    laws: [], // todo
   };
+  console.log(JSON.stringify(data, null, 2));
 
   fetch("https://localhost:8080/postkey_ttpg", {
     method: "POST",
